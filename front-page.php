5807589faf73fc884044464fd6cf0fff2240d222
renem <rene.mejiac@gmail.com>
2016-07-13 10:38:58 -0700

    + Setting up theme, Front page and About us views

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<!-- Page Header -->
+<!-- Set your background image for this header on the line below. -->
+<header class="intro-header front-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="site-heading">
+                    <h1>Front Page</h1>
+                    <hr class="small">
+                    <span class="subheading">A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start BootstrapA Clean Blog Theme by Start Bootstrap A Clean Blog Theme by Start Bootstrap</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+
+if ( ! isset( $content_width ) ) {
+    $content_width = 660;
+}
+function basicTheme_setup (){
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
+    // Register Custom Navigation Walker
+    require_once('wp_bootstrap_navwalker.php');
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'basicTheme' ),
+    ));
+}
+add_action('after_setup_theme', 'basicTheme_setup');
+function basicTheme_scripts (){
+    /* add styles */
+    wp_enqueue_style('bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('font-awesome', get_template_directory_uri() . '/fonts/font-awesome.min.css');
+    wp_enqueue_style('custom', get_template_directory_uri() . '/style.css');
+    /* add scripts */
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), true);
+    wp_enqueue_script('custom', get_template_directory_uri() . '/js/scripts.js', array('bootstrap-js'), true);
+}
+add_action('wp_enqueue_scripts', 'basicTheme_scripts');
+
+function frontPageFeaturedBody(){
+    if(is_front_page()){
+        _e(" id='front-page'");
+    }
+}
+?>
