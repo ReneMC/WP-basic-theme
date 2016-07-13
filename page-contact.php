b4018e1bbd87f268491d70af562f37fd1e97d0d7
renem <rene.mejiac@gmail.com>
2016-07-13 15:47:10 -0700

    + Contact Form 7: setup

--- page-contact.php
+++ page-contact.php
@@ -27,47 +27,43 @@ get_header(); ?>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <p>Want to get in touch with me? Fill out the form below to send me a message and I will try to get back to you within 24 hours!</p>
-                <form name="sentMessage" id="contactForm" novalidate>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label>Name</label>
-                            <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label>Email Address</label>
-                            <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label>Phone Number</label>
-                            <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <div class="row control-group">
-                        <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <label>Message</label>
-                            <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                            <p class="help-block text-danger"></p>
-                        </div>
-                    </div>
-                    <br>
-                    <div id="success"></div>
-                    <div class="row">
-                        <div class="form-group col-xs-12">
-                            <button type="submit" class="btn btn-default">Send</button>
-                        </div>
-                    </div>
-                </form>
+                <?php echo do_shortcode('[contact-form-7 id="35" title="Contact form"]'); ?>
             </div>
         </div>
     </div>
 
     <hr>
+
+
+<!--========= Contact form 7 config ==========-->
+<!--<div class="row control-group">-->
+<!--  <div class="form-group col-xs-12 floating-label-form-group controls">-->
+<!--    <label>Your Name (required)</label><br/>-->
+<!--    [text* your-name class:form-control placeholder "Your Name" id:name]-->
+<!--  </div>-->
+<!--</div>-->
+<!---->
+<!--<div class="row control-group">-->
+<!--  <div class="form-group col-xs-12 floating-label-form-group controls">-->
+<!--    <label>Email Address</label>-->
+<!--    [email* your-email class:form-control id:email placeholder "Your Email Address"]-->
+<!--  </div>-->
+<!--</div>-->
+<!---->
+<!--<div class="row control-group">-->
+<!--  <div class="form-group col-xs-12 floating-label-form-group controls">-->
+<!--    <label>Message</label>-->
+<!--    [textarea your-message class:form-control id:message x5 placeholder "Message"]-->
+<!--  </div>-->
+<!--</div>-->
+<!---->
+<!--<br>-->
+<!---->
+<!--<div class="row">-->
+<!--  <div class="form-group col-xs-12">-->
+<!--    [submit class:btn class:btn-default "Send"]-->
+<!--  </div>-->
+<!--</div>-->
+
 
 <?php get_footer(); ?>
