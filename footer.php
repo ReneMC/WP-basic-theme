9a1d6ec0bfb14e6b2e2fb1d8e8fe67e0af8c0d32
renem <rene.mejiac@gmail.com>
2016-07-13 18:24:01 -0700

    - Footer links

--- footer.php
+++ footer.php
@@ -5,23 +5,23 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <ul class="list-inline text-center">
                     <li>
-                        <a href="#">
+                        <a href="https://www.linkedin.com/in/renemejia" target="_blank">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="https://bitbucket.com/renem" target="_blank">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                <i class="fa fa-bitbucket fa-stack-1x fa-inverse"></i>
                             </span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="https://github.com/ReneMC" target="_blank">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -29,7 +29,7 @@
                         </a>
                     </li>
                 </ul>
-                <p class="copyright text-muted">Copyright &copy; <?php echo get_bloginfo('name'); ?> <?php echo date('Y'); ?></p>
+                <p class="copyright text-muted">Copyright &copy; <?php echo get_bloginfo('name'); ?> <?php echo date('Y'); ?> | Made with ♥ using <a href="https://wordpress.org/">WordPress</a> and <a href="http://startbootstrap.com/">StartBootstrap</a>.</p>
             </div>
         </div>
     </div>
