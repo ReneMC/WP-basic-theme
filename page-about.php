134e3ff1185a7895fe261d794b5b679e577f6b4a
renem <rene.mejiac@gmail.com>
2016-08-03 14:37:11 -0700

    - Page-about: Added portfolio items

--- page-about.php
+++ page-about.php
@@ -32,96 +32,94 @@ get_header(); ?>
         </div>
     </div>
     
+	<!-- Portfolio Item 1 Row -->
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3>Project Description</h3>
+			</div>
+			<div class="panel-body">
+				<div class="col-md-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+					<h3>Project Details</h3>
+					<ul>
+						<li>Lorem Ipsum</li>
+						<li>Dolor Sit Amet</li>
+						<li>Consectetur</li>
+					</ul>
+					<button class="btn btn-default">Ir al sitio</button>
+				</div>
+
+				<div class="col-md-8">
+				   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- /.row -->
+
+
+	<br>
+
+	<!-- Portfolio Item 2 Row -->
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3>Project Description</h3>
+			</div>
+			<div class="panel-body">
+				<div class="col-md-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+					<h3>Project Details</h3>
+					<ul>
+						<li>Lorem Ipsum</li>
+						<li>Dolor Sit Amet</li>
+						<li>Consectetur</li>
+					</ul>
+					<button class="btn btn-default">Ir al sitio</button>
+				</div>
+
+				<div class="col-md-8">
+				   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- /.row -->
+
+	<hr class="small">
+
+
+	<!-- Portfolio Item 3 Row -->
+	<div class="row">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3>Project Description</h3>
+			</div>
+			<div class="panel-body">
+				<div class="col-md-4">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+					<h3>Project Details</h3>
+					<ul>
+						<li>Lorem Ipsum</li>
+						<li>Dolor Sit Amet</li>
+						<li>Consectetur</li>
+					</ul>
+					<button class="btn btn-default">Ir al sitio</button>
+				</div>
+
+				<div class="col-md-8">
+				   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- /.row -->
     
-		<!-- Portfolio Item 1 Row -->
-        <div class="row">
-            <div class="panel panel-default">
-            	<div class="panel-heading">
-            		<h3>Project Description</h3>
-            	</div>
-            	<div class="panel-body">
-					<div class="col-md-4">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-						<h3>Project Details</h3>
-						<ul>
-							<li>Lorem Ipsum</li>
-							<li>Dolor Sit Amet</li>
-							<li>Consectetur</li>
-						</ul>
-						<button class="btn btn-default">Ir al sitio</button>
-					</div>
-
-					<div class="col-md-8">
-					   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
-					</div>
-            	</div>
-            </div>
-
-        </div>
-        <!-- /.row -->
-        
-        
-        <br>
-        
-		<!-- Portfolio Item 2 Row -->
-        <div class="row">
-            <div class="panel panel-default">
-            	<div class="panel-heading">
-            		<h3>Project Description</h3>
-            	</div>
-            	<div class="panel-body">
-					<div class="col-md-4">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-						<h3>Project Details</h3>
-						<ul>
-							<li>Lorem Ipsum</li>
-							<li>Dolor Sit Amet</li>
-							<li>Consectetur</li>
-						</ul>
-						<button class="btn btn-default">Ir al sitio</button>
-					</div>
-
-					<div class="col-md-8">
-					   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
-					</div>
-            	</div>
-            </div>
-
-        </div>
-        <!-- /.row -->
-        
-        <hr class="small">
-        
-        
-		<!-- Portfolio Item 3 Row -->
-        <div class="row">
-            <div class="panel panel-default">
-            	<div class="panel-heading">
-            		<h3>Project Description</h3>
-            	</div>
-            	<div class="panel-body">
-					<div class="col-md-4">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-						<h3>Project Details</h3>
-						<ul>
-							<li>Lorem Ipsum</li>
-							<li>Dolor Sit Amet</li>
-							<li>Consectetur</li>
-						</ul>
-						<button class="btn btn-default">Ir al sitio</button>
-					</div>
-
-					<div class="col-md-8">
-					   <img src="https://unsplash.it/750/500?random" class="img-responsive" alt="Responsive image">
-					</div>
-            	</div>
-            </div>
-
-        </div>
-        <!-- /.row -->
-    
-    
-</div>
+</div><!--.container-->
 
 <hr>
 
