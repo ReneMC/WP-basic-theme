bec35a967e16ff43609d9ccda7c8168a2b1e4d88
rene.mejiac@gmail.com <u81238377@false>
2016-09-01 23:58:44 -0500

    * Fix: Title bug

--- header.php
+++ header.php
@@ -5,8 +5,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     
-    <!-- <title><?php// wp_title(''); ?></title> -->
-    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Rene E. Mejia">
