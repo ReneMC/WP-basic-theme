ac2323c6e5ecdaeb4f33bcd6aa57e3d13cd444e5
Rene <rene.mejiac@gmail.com>
2016-09-24 22:56:24 +0000

    - Footer links: rel nofollow

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <ul class="list-inline text-center">
                     <li>
-                        <a href="https://www.linkedin.com/in/renemejia" target="_blank">
+                        <a href="https://www.linkedin.com/in/renemejia" target="_blank" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="skype:rene.mejiac?chat">
+                        <a href="skype:rene.mejiac?chat" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-skype fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/ReneMC" target="_blank">
+                        <a href="https://github.com/ReneMC" target="_blank" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -29,7 +29,7 @@
                         </a>
                     </li>
                 </ul>
-                <p class="copyright text-muted">Copyright &copy; <?php echo get_bloginfo('name'); ?> <?php echo date('Y'); ?> | Hecho con ♥ utilizando <a <?php frontPageFooterLinks();?> target="_blank" href="https://wordpress.org/">WordPress</a> y <a <?php frontPageFooterLinks();?> target="_blank" href="http://startbootstrap.com/">StartBootstrap</a>.</p>
+                <p class="copyright text-muted">Copyright &copy; <?php echo get_bloginfo('name'); ?> <?php echo date('Y'); ?> | Hecho con ♥ utilizando <a <?php frontPageFooterLinks();?> target="_blank" rel="nofollow" href="https://wordpress.org/">WordPress</a> y <a <?php frontPageFooterLinks();?> target="_blank" rel="nofollow" href="http://startbootstrap.com/">StartBootstrap</a>.</p>
             </div>
         </div>
     </div>
