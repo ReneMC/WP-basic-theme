b08de01c1c6f9817d0172a6e65f472cf65e92e39
Rene <rene.mejiac@gmail.com>
2016-09-26 02:10:55 +0000

    - Footer links: title

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <ul class="list-inline text-center">
                     <li>
-                        <a href="https://www.linkedin.com/in/renemejia" target="_blank" rel="nofollow">
+                        <a href="https://www.linkedin.com/in/renemejia" target="_blank" title="LinkedIn" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="skype:rene.mejiac?chat" rel="nofollow">
+                        <a href="skype:rene.mejiac?chat" title="Chat en Skype" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-skype fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/ReneMC" target="_blank" rel="nofollow">
+                        <a href="https://github.com/ReneMC" target="_blank" title="GitHub" rel="nofollow">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-github fa-stack-1x fa-inverse"></i>
