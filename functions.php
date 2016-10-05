e2a13e8129ec2ee707f94e036cc9a19cfd856d1e
Rene <rene.mejiac@gmail.com>
2016-10-05 02:06:35 +0000

    * REST API head throws W3C error

--- functions.php
+++ functions.php
@@ -49,4 +49,11 @@ function frontPageFooterLinks(){
         _e(" class='white'");
     }
 }
+
+function remove_api () {
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+}
+add_action( ‘after_setup_theme’, 'remove_api' );
 ?>
